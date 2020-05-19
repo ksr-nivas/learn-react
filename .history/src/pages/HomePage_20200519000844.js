b4256f1b446fc5c3dbfe3351bb4073c43ec66e68
Srinivas K <srinivask.ria@gmail.com>
2020-05-19 13:09:56 +0530

    Removed VSCode history files

--- .history/src/pages/HomePage_20200519000844.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HomePage() {
-    return (
-        <React.Fragment>
-            <h1>Hello, welcome to my blog!</h1>
-        </React.Fragment>
-    )
-}
